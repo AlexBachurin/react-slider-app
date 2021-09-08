a50a75dbdafd84d772d5b22d3265ed8482b05e20
Alex <alexbachurin91@gmail.com>
2021-09-08 22:50:43 +0300

    full slider setup, logic left

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-
+import React, { useState } from 'react'
+import { FaQuoteRight } from 'react-icons/fa'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 import './App.css';
+import data from './data'
 
 function App() {
+  const [people, setPeople] = useState(data);
+  //set index for slider
+  const [index, setIndex] = useState(0)
   return (
-    <div className="App">
-      slider
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return <article key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">
+              {quote}
+            </p>
+            <FaQuoteRight className='icon' />
+          </article>
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 }
 
