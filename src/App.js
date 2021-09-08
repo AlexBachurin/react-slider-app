86a9d98a83b7dd18951ddc1b24ba675e59c9a0e7
Alex <alexbachurin91@gmail.com>
2021-09-08 23:05:31 +0300

    simple slider functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,25 @@ function App() {
   const [people, setPeople] = useState(data);
   //set index for slider
   const [index, setIndex] = useState(0)
+
+  //next slide
+  const nextSlide = () => {
+    //check for index boundaries
+    if (index > people.length - 2) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+
+  }
+
+  const prevSlide = () => {
+    if (index === 0) {
+      setIndex(people.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  }
   return (
     <section className="section">
       <div className="title">
@@ -17,8 +36,14 @@ function App() {
       </div>
       <div className="section-center">
         {people.map((person, personIndex) => {
+          let clsNames = 'article';
+          //show active slide based on what index we have in our state,
+          //if it matches with current iterated index of person, then show it
+          if (personIndex === index) {
+            clsNames += ' activeSlide'
+          }
           const { id, image, name, title, quote } = person;
-          return <article key={id}>
+          return <article key={id} className={clsNames}>
             <img src={image} alt={name} className="person-img" />
             <h4>{name}</h4>
             <p className="title">{title}</p>
@@ -28,10 +53,10 @@ function App() {
             <FaQuoteRight className='icon' />
           </article>
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => prevSlide()}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => nextSlide()}>
           <FiChevronRight />
         </button>
       </div>
